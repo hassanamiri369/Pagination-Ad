730e0bcba0d257a1dc9ad00d5e721e7c6b6ef321
Hassanamiri <hassanamiri369@gmail.com>
2022-03-29 16:49:19 +0330

    create pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect} from "react"
+
+import axios from "axios"
+import {Pagination} from "antd";
+import "antd/dist/antd.css"
+
+
 
 function App() {
+
+  const [posts , setPosts] = useState([])
+  const [total , setTotal] = useState("")
+  const [page , setPage ] = useState(1)
+  const [postPerPage , setPostPerPage] = useState(10)
+
+
+
+  useEffect(()=>{
+    const fetchData = async ()=> {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/comments/")
+      setPosts(response.data)
+      setTotal(response.data.length)
+    }
+
+    fetchData()
+  } , [])
+ 
+
+
+  const indexOfLastPage = page + postPerPage;
+  const indexOfFirstPage = indexOfLastPage - postPerPage
+  const currentPosts = posts.slice(indexOfFirstPage , indexOfLastPage)
+
+  const onShowSizeChange = (current , pageSize)=>{
+    setPostPerPage(pageSize)
+  }
+
+
+  const itemRender = (current , type , originalElement)=>{
+      if(type === "prev"){
+        return <a>Previous</a>
+      }
+
+      if(type === "next"){
+        return <a>Next</a>
+      }
+
+      return originalElement
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        
+          <div className="lists">
+          {currentPosts.map(item => (
+            <h3 key={item.id}><span>{item.id}</span> : {item.body}</h3>
+          ))}
+          </div>
+
+
+
+          <div className="pagination">
+          <Pagination
+          onChange={(value)=> setPage(value)}
+          pageSize={postPerPage}
+          total={total}
+          current={page}
+          showSizeChanger
+          showQuickJumper
+          onShowSizeChange={onShowSizeChange}
+          itemRender={itemRender}
+          />
+          </div>
+      </div>
+    </>
   );
 }
 
